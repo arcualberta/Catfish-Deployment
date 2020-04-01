14bfd624f676870a43859e50fbf7f1018255ff1b
Melania Ruaini <melaniar@hotmail.com>
2020-04-01 14:00:22 -0600

    published from master -- April 1 2020

---
+++ Scripts/es6/pages/associationsView.js
@@ -0,0 +1,6 @@
+﻿import React from 'react';
+import ReactDom from 'react-dom';
+import AssociationsLists from '../components/associationsLists';
+
+ReactDom.render(<AssociationsLists />, document.getElementById('associations'));
+
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+﻿const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        associationsView: './Scripts/es6/pages/associationsView.js',
+        collectionsView: './Scripts/es6/pages/collectionsView.js'
+    },
+    output: {
+        path: path.resolve(__dirname, './Scripts/dist'),
+        filename: "[name].js"
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
+    }
+};
