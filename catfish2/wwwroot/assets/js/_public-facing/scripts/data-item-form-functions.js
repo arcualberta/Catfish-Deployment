9a92cb0c687664e6a793c7baefed1e617bb8009a
Kamal Ranaweera <ranaweer@ualberta.ca>
2021-03-18 23:30:44 -0600

    Bug fixes related to editing items.

--- catfish2/wwwroot/assets/js/_public-facing/scripts/data-item-form-functions.js
+++ catfish2/wwwroot/assets/js/_public-facing/scripts/data-item-form-functions.js
@@ -85,3 +85,10 @@ function autoSaveForm(form) {
         }
     });
 }
+
+function createGuid() {
+    function S4() {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    }
+    return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0, 3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+} 
