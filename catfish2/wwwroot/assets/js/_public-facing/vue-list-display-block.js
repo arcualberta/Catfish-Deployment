5f5b9148ace6ffa976d16c1c11506e6e1d70a1d0
Kamal Ranaweera <ranaweer@ualberta.ca>
2021-05-17 17:22:43 -0600

    Updates for the Future of the Past website.

---
+++ catfish2/wwwroot/assets/js/_public-facing/vue-list-display-block.js
@@ -0,0 +1,33 @@
+﻿/*
+    Javascript for the ListDisplay block.
+    See ListDisplayBlock.cshtml or SingleListItem.cs/ListDisplay.cs for more info.
+ */
+
+Vue.component('vue-list-display-block', {
+    props: ["uid", "model"],
+
+    data: function () {
+        return {
+            items: [],
+            selectedItem: null,
+            selectedItemIndex: null
+        }
+    },
+
+    computed: {
+    },
+
+    methods: {
+        selectItem(index) {
+            this.selectedItem = this.model.Items[index];
+            this.selectedItemIndex = index;
+        }
+    },
+
+    created() {
+        console.log("hi", this.model);
+
+        this.items = this.model.Items;
+    },
+
+});
