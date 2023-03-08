8247b5cfe4db6cd95ed0f3a1ca37cba17d334006
KamalRanaweera <ranaweer@ualberta.ca>
2023-03-08 16:26:13 -0700

    Deployed Catfish.Web

---
+++ Catfish.Web/wwwroot/assets/applets/applets.es.js
@@ -0,0 +1,10 @@
+import { b as a, M as n, i as e, F as m, M as r } from "./library-exports.908b918a.js";
+import "vue";
+import "pinia";
+export {
+  a as Apps,
+  n as BootstrapVue3,
+  e as Components,
+  m as FontAwesomeIcon,
+  r as IconsPlugin
+};
