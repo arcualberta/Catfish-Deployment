4fb481c7195a1a82b29833542073491907b073ef
Isurupmw <iwickram@ualberta.ca>
2023-09-21 10:49:46 -0600

    new deployment added

---
+++ dist/assets/Login.f57c2ac9.e13301c0.js
@@ -0,0 +1,1 @@
+import{d as u,E as r,G as i,p as c,H as l,o as p,I as h,h as a,J as d,M as m}from"./index.63d66322.js";const z=u({__name:"Login",setup(f){const t=r(),s=i(),n=c(()=>t.loginResult);l(()=>n.value,async o=>{o!=null&&o.success&&s.push("/")});const e=m.authorizationApiRoot;return(o,g)=>(p(),h(a(d),{"authorization-root":a(e)},null,8,["authorization-root"]))}});export{z as default};
