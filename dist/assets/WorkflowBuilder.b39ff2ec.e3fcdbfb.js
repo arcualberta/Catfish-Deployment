995c7e933afb415be34b9360d3207dcfe2e17e7a
Isurupmw <iwickram@ualberta.ca>
2023-11-27 23:17:56 -0700

    New Home page added

---
+++ dist/assets/WorkflowBuilder.b39ff2ec.e3fcdbfb.js
@@ -0,0 +1,1 @@
+import{d as e,a8 as s,o as t,y as n,x as o,P as i,aa as r}from"./index.ab53c7ed.js";const d=e({__name:"WorkflowBuilder",setup(p){const a=s().params.id;return(c,l)=>(t(),n(o(r),{"pinia-instance":o(i)(),"repository-root":"https://localhost:5020/","workflow-id":o(a)},null,8,["pinia-instance","workflow-id"]))}});export{d as default};
