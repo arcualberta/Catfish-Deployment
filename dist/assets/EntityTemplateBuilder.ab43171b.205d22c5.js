2232df52f3d6cebe85b58e648d3a4bc80c9d0808
Isurupmw <iwickram@ualberta.ca>
2024-01-09 11:32:27 -0700

    few issues fixed

---
+++ dist/assets/EntityTemplateBuilder.ab43171b.205d22c5.js
@@ -0,0 +1,1 @@
+import{d as a,a7 as e,j as o,p as s,u as p,aj as n,a6 as r}from"./index.2a4fbe1a.js";const u=a({__name:"EntityTemplateBuilder",setup(i){e();const t=r.dataRepositoryApiRoot+"/api/entity-templates";return(c,m)=>(o(),s(p(n),{"api-root":t}))}});export{u as default};
---
+++ dist/assets/GoogleCalendar.b210c51d.b25bde3d.js
@@ -0,0 +1,1 @@
+import{d as a,a7 as e,j as o,p as s,u as n,ai as r}from"./index.2a4fbe1a.js";const l=a({__name:"GoogleCalendar",setup(t){return e(),(p,c)=>(o(),s(n(r)))}});export{l as default};
