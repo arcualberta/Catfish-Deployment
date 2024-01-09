ead1a5f8e30895e1d93bdf3b0877b0656f77ecaa
Isurupmw <iwickram@ualberta.ca>
2024-01-09 12:04:34 -0700

    footer fixed

---
+++ dist/assets/Update.845c6fa3.bd64f992.js
@@ -0,0 +1,1 @@
+import{d as n,a7 as r,f as l,j as d,k as p,l as u,s as a,aA as t,m as c,u as m,F as f}from"./index.543944db.js";const B=n({__name:"Update",setup(i){const s=r(),o=l(()=>{var e;return(e=s.params)==null?void 0:e.id});return(e,k)=>(d(),p(f,null,[u("h4",null,[a("Update "),t(e.$slots,"object-type")]),t(e.$slots,"update-delegate"),a(" "+c(m(o)),1)],64))}});export{B as default};
