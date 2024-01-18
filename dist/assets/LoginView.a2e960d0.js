9be8d44551181487935ef4ef375721858954e7c4
Isurupmw <iwickram@ualberta.ca>
2024-01-18 00:53:17 -0700

    model data removed

---
+++ dist/assets/LoginView.a2e960d0.js
@@ -0,0 +1,1 @@
+import{d as n,z as t,j as e,k as a,B as i,u as s,N as r}from"./index.ec558d19.js";const c={class:"login"},m=n({__name:"LoginView",setup(_){return(p,u)=>{const o=t("Login");return e(),a("div",c,[i(o,{authorizationRoot:s(r).authorizationApiRoot},null,8,["authorizationRoot"])])}}});export{m as default};
