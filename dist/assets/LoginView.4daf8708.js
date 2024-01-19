51c437e90e29c943494e00613fc5e37adf095ee9
Isurupmw <iwickram@ualberta.ca>
2024-01-19 15:41:02 -0700

    solr api pointing to test

---
+++ dist/assets/LoginView.4daf8708.js
@@ -0,0 +1,1 @@
+import{d as n,z as t,j as e,k as a,B as i,u as s,N as r}from"./index.444fd584.js";const c={class:"login"},m=n({__name:"LoginView",setup(_){return(p,u)=>{const o=t("Login");return e(),a("div",c,[i(o,{authorizationRoot:s(r).authorizationApiRoot},null,8,["authorizationRoot"])])}}});export{m as default};
