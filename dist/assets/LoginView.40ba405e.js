38580b38737194f90c327cc23f08b4807cb2cc1e
Isurupmw <iwickram@ualberta.ca>
2024-01-23 01:12:06 -0700

    checkbox panel updated minor twerks added

---
+++ dist/assets/LoginView.40ba405e.js
@@ -0,0 +1,1 @@
+import{d as n,y as t,j as e,k as a,A as i,u as s,N as r}from"./index.fff0aaaf.js";const c={class:"login"},m=n({__name:"LoginView",setup(_){return(p,u)=>{const o=t("Login");return e(),a("div",c,[i(o,{authorizationRoot:s(r).authorizationApiRoot},null,8,["authorizationRoot"])])}}});export{m as default};
